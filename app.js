626c3d8d536dd4c76f2758aa9a172376a0a12f46
yamatech <akhr_ymn@outlook.jp>
2022-12-22 13:20:58 +0900

    upgrade all files
    All renewal upgrade! 
    Revamped design, Behavior changes.

--- app.js
+++ app.js
@@ -1,46 +1,38 @@
-const Main = () => {
-  let date = new Date();
-  let clock = document.querySelector('#clock');
-  let today = document.querySelector('#today');
-  clock.innerHTML = date.toLocaleTimeString();
-  today.innerHTML = date.toDateString();
-}
 
-window.onload = () => {
-  setInterval(function () {
-    Main();
-  }, 1000);
-};
+function Main() {
+  const date = new Date();
+  const [hour, minutes, seconds] = [date.getHours(), date.getMinutes(), date.getSeconds()];
+  const clock = document.querySelector('.clock');
+  const clocksec = document.querySelector('.sec');
+  const today = document.querySelector('.footer');
+
+  let min = minutes;
+  let sec = seconds;
+  if (minutes < 10) { min = `0${minutes}`; }
+  if (seconds < 10) { sec = `0${seconds}`; }
+
+  clock.innerHTML = `${hour}:${min}`;
+  clocksec.innerHTML = `:${sec}`;
+  today.innerHTML = date.toDateString();
+
+  requestAnimationFrame(Main);
+}
+Main();
 
 
 // Change stylesheet
 const changeCSS = document.getElementById('changeCSS');
 
-changeCSS.addEventListener('click', function () {
+changeCSS.addEventListener('click', () => {
 
   document.body.classList.toggle('light-mode');
   document.body.classList.toggle('dark-mode');
 
   const className = document.body.className;
-  if(className == 'light-mode') {
-    this.textContent = "🔅";
+  if (className == 'light-mode') {
+    changeCSS.src = "src/icon1.svg";
   } else {
-    this.textContent = "🔆";
+    changeCSS.src = "src/icon2.svg";
   }
 
-  console.log('current className: ' + className);
-
 });
-
-
-/* 
-
-memo:
-
-const date = new Date();
-
-const [month, day, year] = [date.getMonth(), date.getDate(), date.getFullYear()];
-
-const [hour, minutes, seconds] = [date.getHours(), date.getMinutes(), date.getSeconds()];
-
- */
