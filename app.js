566b9613213618d81ceba02c26a6b022ae57d738
yamatech <akhr_ymn@me.com>
2023-03-27 18:03:35 +0900

    change "app.js"

--- app.js
+++ app.js
@@ -1,39 +1,39 @@
 'use strict';
 
-function Main() {
+function updateClock() {
+  const clockEl = document.querySelector('.clock');
+  const secondsEl = document.querySelector('.sec');
+  const todayEl = document.querySelector('.today');
 
-  const $clock = document.querySelector('.clock');
-  const $seconds = document.querySelector('.sec');
-  const $today = document.querySelector('.today');
+  const date = new Date();
+  const hour = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
 
-  let date = new Date();
-  let [hour, minutes, seconds] = [
-    date.getHours(), date.getMinutes(), date.getSeconds()
-  ];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
 
-  if (minutes < 10) minutes = `0${minutes}`;
-  if (seconds < 10) seconds = `0${seconds}`;
-
-  $clock.innerHTML = `${hour}:${minutes}`;
-  $seconds.innerHTML = `:${seconds}`;
-  $today.innerHTML = date.toDateString();
-
-  requestAnimationFrame(Main);
-
+  clockEl.textContent = `${hour}:${minutes}`;
+  secondsEl.textContent = `:${seconds}`;
+  todayEl.textContent = date.toDateString();
 }
 
-Main();
+setInterval(updateClock, 1000);
 
 
 // Change stylesheet
-const $changeCSS = document.getElementById('changeCSS');
+const changeCSS = document.getElementById('changeCSS');
 
-$changeCSS.addEventListener('click', () => {
+changeCSS.addEventListener('click', () => {
 
   document.body.classList.toggle('light-mode');
   document.body.classList.toggle('dark-mode');
 
-  const $className = document.body.className;
-  $className === 'light-mode' ? $changeCSS.src = "src/icon1.svg" : $changeCSS.src = "src/icon2.svg";
+  const className = document.body.className;
+  className === 'light-mode' ? changeCSS.src = "src/icon1.svg" : changeCSS.src = "src/icon2.svg";
 
 });
